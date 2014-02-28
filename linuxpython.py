7728a290c500df0c01c2bac5e11953dd49480128
Anderson Rocha <noiseand@gmail.com>
2014-02-28 10:32:05 -0300

    Create linuxpython.py

---
+++ linuxpython.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import serial
+import os, glob
+
+SEND_COMMAND_CODE = 84, 254
+ACKNOWLEDGE_CODE = 85, 255, 170
+
+#2 bytes commands:
+BEEP_CODE = 196, 0
+
+#========================================================================================================#
+
+
+if __name__ == '__main__':
+    comunicSerial = serial.Serial(None, 9600, bytesize=serial.EIGHTBITS, \
+                                                       parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, \
+                                                   timeout=0.1, xonxoff=0, rtscts=0)
+    comunicSerial.port = "/dev/ttyACM0"
+    comunicSerial.open()
+    command = SEND_COMMAND_CODE + BEEP_CODE
+    s = ""
+    for i in command:
+        s = s + chr(i)
+    comunicSerial.write(s)
