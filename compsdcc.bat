3ae8dcb5792b6dfb39f84de47f05760a18f5c48f
Anderson Rocha <noiseand@gmail.com>
2014-02-28 09:46:42 -0300

    Update compsdcc.bat

--- compsdcc.bat
+++ compsdcc.bat
@@ -1,1 +1,5 @@
-rem
+@echo off
+
+sdcc -mpic16 -p18f4550 --mplab-comp -c main.c --code-loc 0x1840
+sdcc -mpic16 -p18f4550 --code-loc 0x1840 --no-crt main.o libc18f.lib libm18f.lib libio18f4550.lib -m -o main.hex
+pause
