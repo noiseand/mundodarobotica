be28183b5d54056815e0387de7926264a6665a75
Anderson Rocha <noiseand@gmail.com>
2014-02-28 10:10:09 -0300

    Update comphitec.bat

--- comphitec.bat
+++ comphitec.bat
@@ -1,1 +1,5 @@
-rem
+@echo off
+
+rem "C:\arquiv~1\HI-TECH Software\PICC\std\9.60\bin\picc.exe" --chip=18f4550 main.c
+"C:\Arquivos de programas\HI-TECH Software\PICC-18\PRO\9.65\bin\picc18.exe" --chip=18f4550 main.c --CODEOFFSET=0x1840
+pause
