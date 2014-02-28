d69400d13452b7efb91bfd2e154a043e8c293591
Anderson Rocha <noiseand@gmail.com>
2014-02-28 09:49:09 -0300

    Update compsdcc.sh

--- compsdcc.sh
+++ compsdcc.sh
@@ -1,1 +1,4 @@
-#
+#sdcc --ivt-loc=0x1840 -mpic16 -p18f4550 -c main.c
+sdcc --ivt-loc=0x1840 -c --use-non-free -mpic16 -p18f4550 main.c
+
+sdcc -mpic16 -p18f4550 --ivt-loc=0x1840 --no-crt main.o libc18f.lib libm18f.lib libio18f4550.lib -m -o main.hex
