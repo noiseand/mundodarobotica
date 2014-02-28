36156e9ec82559aad65502c0d81c3d61ae3f22d7
Anderson Rocha <noiseand@gmail.com>
2014-02-28 09:33:13 -0300

    Create compc18libs.bat

---
+++ compc18libs.bat
@@ -0,0 +1,7 @@
+"C:\MCC18\bin\mcc18.exe" -p=18F4550 "c018.c" -fo="c018.o" -D__DEBUG -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa-
+
+"C:\MCC18\bin\mcc18.exe" -p=18F4550 "c018i.c" -fo="c018i.o" -D__DEBUG -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa-
+
+"C:\MCC18\bin\mcc18.exe" -p=18F4550 "c018_e.c" -fo="c018_e.o" -D__DEBUG -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa-
+
+"C:\MCC18\bin\mcc18.exe" -p=18F4550 "c018i_e.c" -fo="c018i_e.o" -D__DEBUG -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa-
