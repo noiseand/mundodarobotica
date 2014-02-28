e9ce6aab042832d37f52181ae2f1c1436a3acd00
Anderson Rocha <noiseand@gmail.com>
2014-02-28 10:29:58 -0300

    Create windowsphp.php

---
+++ windowsphp.php
@@ -0,0 +1,14 @@
+<?php
+
+$fp = fopen("COM3", "w");
+
+fwrite($fp, chr(84));
+fwrite($fp, chr(254));
+fwrite($fp, chr(196));
+fwrite($fp, chr(0));
+fwrite($fp, chr(13));
+fwrite($fp, chr(10));
+
+fclose($fp);
+
+?>
