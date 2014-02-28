2d551dd4c3838be6ef8fe1725d94b95ffb6d2744
Anderson Rocha <noiseand@gmail.com>
2014-02-28 10:09:45 -0300

    Update hitecmain.c

--- hitecmain.c
+++ hitecmain.c
@@ -1,19 +1,19 @@
-     1: #include<htc.h>
-     2: 
-     3: void main()
-     4: {
-     5: 
-     6: TRISB = 0b00000011;
-     7: TRISA = 0b00101111;
-     8: TRISC = 0b00000000;
-     9: TRISD = 0b00000010;
-    10: TRISE = 0b00000111;
-    11: 
-    12: while (1){
-    13:         RC2 = 1;
-    14:         _delay(100000);
-    15:         RC2 = 0;
-    16:         _delay(100000);
-    17: }
-    18: 
-    19: }
+#include<htc.h>
+
+void main()
+{
+
+TRISB = 0b00000011;
+TRISA = 0b00101111;
+TRISC = 0b00000000;
+TRISD = 0b00000010;
+TRISE = 0b00000111;
+
+while (1){
+	RC2 = 1;
+	_delay(100000);
+	RC2 = 0;
+	_delay(100000);
+}
+
+}
